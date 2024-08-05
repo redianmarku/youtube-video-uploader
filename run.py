0e7eddd3864249c4a8beffbfc3597b04a8414456
Redian Marku <redian1marku@gmail.com>
2024-08-05 15:07:14 +0200

    Initial Commit

---
+++ run.py
@@ -0,0 +1,64 @@
+import os
+import google_auth_httplib2
+import google_auth_oauthlib
+import googleapiclient.discovery
+import googleapiclient.errors
+import googleapiclient.http
+
+SCOPES = ["https://www.googleapis.com/auth/youtube.upload"]
+TOKEN_FILE = 'token.json'
+
+def authenticate_youtube():
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    if os.path.exists(TOKEN_FILE):
+        os.remove(TOKEN_FILE)
+
+    # Load client secrets file
+    client_secrets_file = "/Users/redi/Desktop/youtube-uplaod/client.json"
+
+    # Get credentials and create an API client
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        client_secrets_file, SCOPES)
+    credentials = flow.run_local_server()
+
+    youtube = googleapiclient.discovery.build(
+        "youtube", "v3", credentials=credentials)
+
+    return youtube
+
+def upload_video(youtube):
+    request_body = {
+        "snippet": {
+            "categoryId": "22",
+            "title": "Uploaded from Python",
+            "description": "This is the most awsome description ever",
+            "tags": ["test","python", "api" ]
+        },
+        "status":{
+            "privacyStatus": "private"
+        }
+    }
+
+    media_file = "/Users/redi/Desktop/youtube-uploader/video.mp4"
+
+    request = youtube.videos().insert(
+        part="snippet,status",
+        body=request_body,
+        media_body=googleapiclient.http.MediaFileUpload(media_file, chunksize=-1, resumable=True)
+    )
+
+    response = None 
+
+    while response is None:
+        status, response = request.next_chunk()
+        if status:
+            print(f"Upload {int(status.progress()*100)}%")
+
+        print(f"Video uploaded with ID: {response['id']}")
+
+if __name__ == "__main__":
+    youtube = authenticate_youtube()
+    upload_video(youtube)
+
+
